9a806d263cacd00e4a5caed6d9d1240f8d43c0c5
danthespal <55256796+danthespal@users.noreply.github.com>
2022-09-10 21:20:33 +0300

    template

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void displayHomeScreen()
+{
+	cout << "1. Update School Data Recors\t\t\t\t\t\t\t\2. View School Records";
+	cout << "\na. Add or remove student\t\t\t\t\t\t\td. View student database";
+	cout << "\nb. Add or remove class\t\t\t\t\t\t\te. View staff database";
+	cout << "\nc. Add or remove school staff\t\t\t\t\t\t\tf. View class database";
+	cout << "\n\n3. Accounting and Finance";
+	cout << "\ng. Receive student fee";
+	cout << "\nh. Pay salary to school staff";
+	cout << "\ni. View last five transactions";
+}
+
+int main()
+{
+	displayHomeScreen();
+}
