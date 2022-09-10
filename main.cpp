01435bc1b303ada389ade46f44ef2fb206f080f3
danthespal <55256796+danthespal@users.noreply.github.com>
2022-09-10 22:21:10 +0300

    feature: rollNumber generator
    - roll number se va genera automat, bazat pe clasa si sectia din care face parte
    - se va generate in ordine alfabetica si va fi unic pentru fiecare student
    - e.g. 12252 reprezinta clasa 12th, sectia 'B' are pozitia 52 in alfabet

--- main.cpp
+++ main.cpp
@@ -1,129 +1,148 @@
-#include <iostream>		// for basic input/output operations
-#include <string>		// for strings objects
-#include <fstream>		// for data file handling
+#include <iostream>     // for basic input and output operations e.g. cin, cout, etc.
+#include <fstream>      // for data file handling
+#include <string>       // for string objects
+using namespace std;    // declares std namespace. so, we don't need to add 'std::' before cout, cin and other standard library methods
 
-using namespace std;	// declares std namespace as per latest C++ standard
-
-int generateRollNumber();
-
-
-// student class has all the data members and required functions for students
+// student class has all the data members and required member functions for students
 class Student
 {
 private:
-	int rollNumber = 0, studentGroup = 0; // studentGroup is the class or standard of student (for e.g. class 12th)
-	string studentName, motherName, fatherName;
-	char studentSection;
+    int rollNumber, studentClass{};
+    char studentName[29]{}, studentSection{};
+    string motherName, fatherName;
 
 public:
-	void inputStudentDetails();
-	//int generateRollNumber();
+    Student() // constructor
+    {
+        rollNumber = 1;
+    }
+    void inputStudentDetails();
+    int generateRollNumber();
 };
+
+// generate and return student roll number in format - 12252 which is equivalent to 12th 'B' 52
+// we generate roll number alphabetically by using the first three characters of name.
+int Student::generateRollNumber()
+{
+    Student schoolStudentTemp;
+    fstream studentFile("data/student.dat", ios::in | ios::out | ios::app | ios::binary);
+    short flag = 0;
+
+    while (studentFile.read((char*)&schoolStudentTemp, sizeof(schoolStudentTemp)))
+    {
+        if (studentClass == schoolStudentTemp.studentClass && tolower(studentSection) == tolower(schoolStudentTemp.studentSection))
+        {
+            flag++;
+            for (short i = 0; i < 3; ++i)
+            {
+                if (tolower(schoolStudentTemp.studentName[i]) > tolower(studentName[i]))
+                {
+                    schoolStudentTemp.rollNumber++;
+                    rollNumber = schoolStudentTemp.rollNumber - 1;
+                    break;
+                }
+                else if (tolower(schoolStudentTemp.studentName[i]) < tolower(studentName[i]))
+                {
+                    rollNumber = schoolStudentTemp.rollNumber + 1;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+    if (flag == 0)
+    {
+        rollNumber = (studentClass * 1000) + (tolower(studentSection) - 96) * 100 + rollNumber;
+    }
+    return rollNumber;
+}
 
 void Student::inputStudentDetails()
 {
-	char ch;
-	cin.get(ch); // fixes error of skipping input
-	cout << "\nEnter student name: ";
-	getline(cin, studentName);
-	cout << "Enter the class (1 to 12): ";
-	cin >> studentGroup;
-	cout << "Enter the section ('A' to 'D'): ";
-	cin.get(ch);
-	cin.get(studentSection);
-	cout << "Enter student father's name: ";
-	cin.get(ch);
-	getline(cin, fatherName);
-	cout << "Enter student mother's name: ";
-	getline(cin, motherName);
-	/*
-	rollNumber = generateRollNumber();
-	cout << "Generated roll number = " << rollNumber;
-	*/
+    system("cls");
+    cin.ignore(); // fixes skipping of input; this will be used throughout the code
+    cout << "\nEnter student name (max. 28 characters): ";
+    gets_s(studentName);
+    cout << "Enter the class (1 to 12): ";
+    cin >> studentClass;
+    cout << "Enter the section ('A' to 'D'): ";
+    cin.ignore();
+    cin.get(studentSection);
+    cout << "Enter student father's name: ";
+    cin.ignore();
+    getline(cin, fatherName);
+    cout << "Enter student mother's name: ";
+    getline(cin, motherName);
+    cout << "\nGenerated roll number is " << generateRollNumber() << ". Please note it in a safe place for future reference.";
 }
-
-// Generate and return student roll number in format - 12252 which is equivalent to 12th 'B' 52
-/* ROLL NUMBER SYSTEM IS UNDER PROGRESS!
-int generateRollNumber()
-{
-	fstream studentFile("data/student.dat", ios::in | ios::out | ios::app | ios::binary);
-	studentFile.seekg(0); //places cursor to the beginning of file
-	int standard = schoolStudent.studentGroup;
-	int section = tolower(schoolStudent.studentSection) - 96;
-	int flag = 1;
-	long roll = 0;
-	while (!studentFile.eof())
-	{
-		studentFile.read((char *)&schoolStudent, sizeof(schoolStudent));
-		if (standard == schoolStudent.studentGroup && section == schoolStudent.studentSection)
-			flag++;
-	 //RESUME HERE! WORK NEEDS TO BE DONE!
-	}
-	roll = ((standard * 1000) + (section * 100) + flag);
-	studentFile.close();
-	return roll;
- }
- */
 
 void addStudent()
 {
-	Student schoolStudent;
-	fstream studentFile("data/student.dat", ios::in | ios::out | ios::app | ios::binary);
-	schoolStudent.inputStudentDetails();
-	studentFile.write((char*)&schoolStudent, sizeof(schoolStudent));
-	studentFile.close();
+    Student schoolStudent;
+    fstream studentFile("data/student.dat", ios::in | ios::out | ios::app | ios::binary);
+    schoolStudent.inputStudentDetails();
+    studentFile.write((char*)&schoolStudent, sizeof(schoolStudent));
+    studentFile.close();
 }
 
+// displays home screen only if the administrator has entered the correct credentials
+// note: Administrator is anyone using our school management system app
 void HomeScreen()
 {
-	system("cls");
-	cout << "1. ADD OR REMOVE DATA RECORDS\t\t\t\t\t\t\t2. VIEW DATA RECORDS";
-	cout << "\na. Add or remove student\t\t\t\t\t\t\td. View student database";
-	cout << "\nb. Add or remove class\t\t\t\t\t\t\t\te. View staff database";
-	cout << "\nc. Add or remove school staff\t\t\t\t\t\t\tf. View class database";
-	cout << "\n\n3. ACCOUNTING AND FINANCE\t\t\t\t\t\t\t4. UPDATE OR MODIFY DATA RECORDS";
-	cout << "\ng. Receive student fee\t\t\t\t\t\t\t\tj. Update student data";
-	cout << "\nh. Pay salaray to school staff\t\t\t\t\t\t\tk. Update class data";
-	cout << "\ni. View last five transactions\t\t\t\t\t\t\tl. Update staff data";
-	cout << "\n\nEnter appropiate menu option to access that functionality. For e.g. press 'A' to Add or remove a student: ";
-	char menuOption;
-	cin.get(menuOption);
-	switch (menuOption)
-	{
-	case 'a':
-		addStudent();
-		break;
-	case 'b':
-		cout << "\nWork under progress...";
-		break;
-	case 'c':
-		cout << "\nWork under progress...";
-		break;
-	case 'd':
-		cout << "\nWork under progress...";
-		break;
-	case 'e':
-		cout << "\nWork under progress...";
-		break;
-	case 'f':
-		cout << "\nWork under progress...";
-		break;
-	case 'g':
-		cout << "\nWork under progress...";
-		break;
-	case 'h':
-		cout << "\nWork under progress...";
-		break;
-	case 'i':
-		cout << "\nWork under progress...";
-		break;
-	default:
-		cout << "\nWrong Input Received.";
-	}
+    system("cls");
+    cout << "1. ADD OR REMOVE DATA RECORDS\t\t\t\t\t\t\t2. VIEW DATA RECORDS";
+    cout << "\nA. Add or remove a student\t\t\t\t\t\t\tD. View student database";
+    cout << "\nB. Add or remove a teacher\t\t\t\t\t\t\tE. View teacher database";
+    cout << "\nC. Add or remove a school staff\t\t\t\t\t\t\tF. View staff database";
+    cout << "\n\n3. ACCOUNTING AND FINANCE\t\t\t\t\t\t\t4. UPDATE OR MODIFY DATA RECORDS";
+    cout << "\nG. Receive student fee\t\t\t\t\t\t\t\tJ. Update student data";
+    cout << "\nH. Pay salaray to school staff or teacher\t\t\t\t\tK. Update class data";
+    cout << "\nI. View last five transactions and account balance\t\t\t\tL. Update staff data";
+    cout << "\n\n=> Enter the displayed menu option of your choice to proceed. For e.g. Press 'A' to 'Add or remove a student': ";
+    char menuOption;
+    cin.get(menuOption);
+    switch (tolower(menuOption))
+    {
+    case 'a':
+        addStudent();
+        break;
+    case 'b':
+        cout << "\nWork under progress...";
+        break;
+    case 'c':
+        cout << "\nWork under progress...";
+        break;
+    case 'd':
+        cout << "\nWork under progress...";
+        break;
+    case 'e':
+        cout << "\nWork under progress...";
+        break;
+    case 'f':
+        cout << "\nWork under progress...";
+        break;
+    case 'g':
+        cout << "\nWork under progress...";
+        break;
+    case 'h':
+        cout << "\nWork under progress...";
+        break;
+    case 'i':
+        cout << "\nWork under progress...";
+        break;
+    default:
+        cout << "\nWrong Input Received.";
+    }
 }
 
+/**
+ * main() starts here
+*/
 int main()
 {
-	HomeScreen();
-	return 0;
+    HomeScreen();
+    return 0;
 }
